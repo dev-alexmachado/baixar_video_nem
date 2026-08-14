0c5707750088501d7f00d66bfe632ec11c9bc1aa
dev-alexmachado <alexmachado.ribeiro@gmail.com>
2026-08-14 17:11:14 -0300

    Updated: versão desktop.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request
+import os
+import sys
+import webview
 import yt_dlp
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# Configuração de caminhos para o PyInstaller
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html")
 
@@ -19,5 +28,40 @@ def download():
     except Exception as e:
         return f"Erro ao tentar baixar vídeo: {str(e)}.{voltar}"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Cria a janela desktop customizada apontando para o servidor Flask
+    # Você pode definir o título, largura (width) e altura (height)
+    window = webview.create_window(
+        title="Download de vídeos", 
+        url=app, 
+        width=1000, 
+        height=700
+    )
+    
+    # Inicia a janela (o pywebview inicia o Flask internamente de forma automática)
+    webview.start()
+
+
+# from flask import Flask, render_template, request
+# import yt_dlp
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+# @app.route("/download", methods=['POST'])
+# def download():
+#     voltar = '<br><a href="/"><b>Voltar</b></a>'
+#     try:
+#         url = request.form.get('url')
+#         ydl_opts = {}
+#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#             ydl.download(url)
+#         return f"Download concluído!{voltar}"
+#     except Exception as e:
+#         return f"Erro ao tentar baixar vídeo: {str(e)}.{voltar}"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
